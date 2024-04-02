c559a4749113612e5a5d84122216fa869766446f
Vasu Jain <vasu.jain333@gmail.com>
2024-04-02 23:26:27 +0530

    Hello

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memory-board-server-1.onrender.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
