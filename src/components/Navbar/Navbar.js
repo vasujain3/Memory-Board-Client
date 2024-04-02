382e938d191a434d2fbd9af291707c6f729169a7
Vasu Jain <vasu.jain333@gmail.com>
2024-04-02 22:54:18 +0530

    Hello

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -47,6 +47,7 @@ const Navbar = () => {
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout} as="a" href="https://khumar.notion.site/61959803c693449fa2a6c61dedd83ee7?v=08eabf6fc76c496ca6974ec9aef875c5">Calender</Button>
             <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
